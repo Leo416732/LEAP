892aa48dfc922c8c4ce08dbe116bed74c5931d43
Morning <morning@admins-iMac.local>
2022-12-08 08:43:19 +0800

    methods

---
+++ JS/Methods/methods.js
@@ -0,0 +1,33 @@
+//
+let a = [1, 2, 2, 3, 7, 5, 5, 80, 454];
+console.log(a.join());
+console.log(a.includes(1));
+console.log(a.at(1));
+
+//pop
+console.log(a.pop());
+console.log(a);
+
+//push hamgiin svvld nemn
+console.log(a.push(9));
+
+//shift
+console.log(a.shift(1));
+console.log(a);
+
+//sort
+console.log(
+  a.sort((a, b) => {
+    return a - b;
+  })
+);
+
+//slice
+
+//reverse
+
+//splice
+
+//replace
+
+//replaceAll
