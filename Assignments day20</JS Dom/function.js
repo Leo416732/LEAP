20c27020dc5dcc8c32a9d675bb92c0a8d69c4087
Munkh Suld <munkhsuld@Munkhs-MacBook-Air.local>
2022-12-12 14:59:47 +0800

    one click

---
+++ Assignments day20</JS Dom/function.js
@@ -0,0 +1,9 @@
+console.log(document.getElementById("aimag-101"));
+console.log(document.getElementById("aimag-101"));
+console.log(document.getElementById("aimag-315"));
+document.getElementById("aimagNames").style.padding = "100px";
+document.getElementById("aimagNames").style.margin = "100px";
+document.getElementById("aimagNames").style.color = "grey";
+document.getElementById("aimagNames").style.fontSize = "30px";
+document.getElementById("aimagNames").style.border = "3px solid black";
+
