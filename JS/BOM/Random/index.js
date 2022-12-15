45f7c9fce786fa21a8c8b400609955b6ed22d435
Morning <morning@admins-iMac.local>
2022-12-15 08:43:14 +0800

    d

--- JS/BOM/Random/index.js
+++ JS/BOM/Random/index.js
@@ -1,36 +1,73 @@
+// score
 let score = 0;
+let scored = document.getElementById("score");
+scored.innerHTML = `Level: ${score}`;
+scored.style.color = "white";
+scored.style.textAlign = "center";
+scored.style.padding = "30px 0";
 
+//root
 let rootElement = document.getElementById("root");
 let rootSec = document.getElementById("root");
+document.querySelector("body").style.backgroundColor = "#001f24";
 rootSec.style.display = "flex";
+rootSec.style.margin = "auto";
+rootSec.style.gap = "7px";
 rootSec.style.flexWrap = "wrap";
-rootSec.style.width = "440px";
-let randomNum = Math.floor(Math.random() * 16 + 1);
-let colorID;
+rootSec.style.width = "428px";
+rootSec.style.border = "2px solid black";
+rootElement.style.padding = "5px";
+
+//start game
 function startGame() {
+  let randomR = Math.floor(Math.random() * 255);
+  let randomG = Math.floor(Math.random() * 255);
+  let randomB = Math.floor(Math.random() * 255);
+  let randomNum = Math.floor(Math.random() * 16);
+  rootElement.innerHTML = "";
+  // for loop
   for (i = 0; i < 16; i++) {
-    let randomR = Math.floor(Math.random() * 255 + 1);
-    let randomG = Math.floor(Math.random() * 255 + 1);
-    let randomB = Math.floor(Math.random() * 255 + 1);
     let newDiv = document.createElement("div");
-    let g = rootElement.appendChild(newDiv);
+    rootElement.appendChild(newDiv);
     newDiv.style.width = "100px";
     newDiv.style.height = "100px";
     newDiv.style.border = "1px solid black";
+    let colorChange =
+      (newDiv.style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`);
     let different = i == randomNum;
+
+    // different
+    if (different) {
+      if (randomR > 100) {
+        colorChange = newDiv.style.backgroundColor = `rgb(${randomR - 40},${
+          randomG - 40
+        },${randomB - 40})`;
+      } else if (randomR < 100 && randomR > 0) {
+        colorChange = newDiv.style.backgroundColor = `rgb(${randomR + 40},${
+          randomG + 40
+        },${randomB + 40})`;
+      }
+    }
+    // click
     newDiv.addEventListener("click", function () {
       if (different) {
         score += 1;
-        console.log(score);
+        startGame();
+        scored.innerHTML = `Level: ${score}`;
+        if (score > 10 && different) {
+          colorChange = newDiv.style.backgroundColor = `rgb(${
+            randomR - 20
+          },${randomG},${randomB - 20})`;
+          console.log(colorChange);
+        } else if (score > 20 && different) {
+          colorChange =
+            newDiv.style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
+        }
       } else {
         score = 0;
+        scored.innerHTML = `Level: ${score}`;
       }
     });
-    if (different) {
-      newDiv.style.backgroundColor = `rgb(${randomR},${randomB},${randomG})`;
-    } else {
-      newDiv.style.backgroundColor = `rgb(${randomR},${randomB},${randomG})`;
-    }
   }
 }
 startGame();
