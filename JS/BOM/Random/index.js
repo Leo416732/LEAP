b517b273cf6ef7f50f7f324d41c8bac1b2a012df
Munkh Suld <munkhsuld@Munkhs-MacBook-Air.local>
2022-12-21 20:05:02 +0800

    game

--- JS/BOM/Random/index.js
+++ JS/BOM/Random/index.js
@@ -1,7 +1,7 @@
 const root = document.querySelector(".root");
 const score = document.querySelector("#score");
-const reset = document.querySelector(".resBtn");
-const tiles = 9;
+const resetD = document.querySelector(".resBtn");
+const tiles = 16;
 let newScore = 0;
 function randomNumber(min, max) {
   return Math.floor(Math.random() * max + min);
@@ -9,7 +9,6 @@ function randomNumber(min, max) {
 
 function updateScore(point) {
   newScore += point;
-  score.innerHTML = newScore;
 }
 
 function rgbGenerator() {
@@ -19,13 +18,62 @@ function rgbGenerator() {
   return [r, g, b];
 }
 
+//easy mode background color
 function getDifferentColor(colors) {
   const newColor = [...colors];
   const random = randomNumber(0, 2);
-  if (newColor[random] > 150) {
-    newColor[random] -= 40;
+  if (newColor[random] > 100) {
+    newColor[random] -= 50;
   } else {
     newColor[random] += 50;
+  }
+  return `rgb(${newColor[0]},${newColor[1]},${newColor[2]})`;
+}
+
+//normal mode background color
+function getDifferentColorNormal(colors) {
+  const newColor = [...colors];
+  const random = randomNumber(0, 2);
+  if (newColor[random] > 100) {
+    newColor[random] -= 30;
+  } else {
+    newColor[random] += 30;
+  }
+  return `rgb(${newColor[0]},${newColor[1]},${newColor[2]})`;
+}
+
+//Hard mode background color
+function getDifferentColorHard(colors) {
+  const newColor = [...colors];
+  const random = randomNumber(0, 2);
+  if (newColor[random] > 100) {
+    newColor[random] -= 20;
+  } else {
+    newColor[random] += 20;
+  }
+  return `rgb(${newColor[0]},${newColor[1]},${newColor[2]})`;
+}
+
+//expert mode background color
+function getDifferentColorExport(colors) {
+  const newColor = [...colors];
+  const random = randomNumber(0, 2);
+  if (newColor[random] > 100) {
+    newColor[random] -= 10;
+  } else {
+    newColor[random] += 10;
+  }
+  return `rgb(${newColor[0]},${newColor[1]},${newColor[2]})`;
+}
+
+//God mode background color
+function getDifferentColorGod(colors) {
+  const newColor = [...colors];
+  const random = randomNumber(0, 2);
+  if (newColor[random] > 100) {
+    newColor[random] -= 5;
+  } else {
+    newColor[random] += 5;
   }
   return `rgb(${newColor[0]},${newColor[1]},${newColor[2]})`;
 }
@@ -33,14 +81,43 @@ function startGame() {
   root.innerHTML = "";
   const colors = rgbGenerator();
   const randomIndex = randomNumber(0, 8);
+  score.innerHTML = `Max score: ${newScore}`;
 
   for (i = 0; i < tiles; i++) {
     const box = document.createElement("div");
     box.className = "list";
     root.appendChild(box);
     let isDifferent = randomIndex == i;
+
+    //different
     if (isDifferent) {
       box.style.backgroundColor = getDifferentColor(colors);
+
+      //easy mode 
+      if(newScore > 0 && newScore < 3){
+        console.log(`easy`)
+        box.style.backgroundColor = getDifferentColor(colors);
+      }
+      //normal mode
+      else if(newScore > 3 && newScore < 6){
+        console.log("normal")
+        box.style.backgroundColor = getDifferentColorNormal(colors);
+      }
+      //hard mode
+      else if(newScore > 6 && newScore < 10){
+        console.log("hard")
+        box.style.backgroundColor = getDifferentColorHard(colors);
+      }
+      //export mode
+      else if(newScore > 10 && newScore < 30){
+        console.log("hard")
+        box.style.backgroundColor = getDifferentColorExport(colors);
+      }
+      //god mode
+      else if(newScore > 30 && newScore < 1000){
+        console.log("god")
+        box.style.backgroundColor = getDifferentColorGod(colors);
+      }
     } else {
       box.style.backgroundColor = `rgb(${colors[0]},${colors[1]},${colors[2]})`;
     }
@@ -48,12 +125,11 @@ function startGame() {
       if (isDifferent) {
         updateScore(1);
         startGame();
-      } else {
+      }else {
         newScore = 0;
-        score.innerHTML = `Max score: ${newScore}`;
         if (newScore == 0) {
           root.innerHTML =
-            "<img class='image' src='https://cdn-icons-png.flaticon.com/512/7371/7371972.png'><button class='resBtn'>Reset</button>";
+            "<img class='image' src='https://cdn-icons-png.flaticon.com/512/7371/7371972.png'><button onclick='resetFunction()' class='resBtn'>Reset</button>";
         }
       }
     });
@@ -61,9 +137,7 @@ function startGame() {
 }
 
 startGame();
-function res() {
-  reset.addEventListener("click", () => {
-    startGame();
-  });
+
+function resetFunction(){
+  startGame()
 }
-res();
--- JS/DOM Project/first.js
+++ JS/DOM Project/first.js
@@ -1,7 +1,7 @@
 let body = document.querySelector("body");
 
 // title
-let title = document.getElementById("first");
+let title = document.getElementById("textTitle");
 title.innerHTML = "Todo List App";
 
 // clock
@@ -19,39 +19,32 @@ let button = document.querySelector("#btn");
 let div = document.getElementsByClassName("div")[0];
 
 // click
-button.addEventListener("click", function () {
-  let crt;
-  let click;
-  if (inputElement.value == "") {
-  } else {
-    crt = document.createElement("div");
-    crt.innerHTML = `${inputElement.value}<img class ='image close' src = 'https://cdn-icons-png.flaticon.com/128/1828/1828843.png'> <img class='image' src='https://cdn-icons-png.flaticon.com/128/5290/5290999.png'/> <img class='image pen' src='https://cdn-icons-png.flaticon.com/128/1250/1250615.png'/>`;
-    click = crt.className = "click";
-  }
-  if (inputElement.value == inputElement.value) {
-    inputElement.value = "";
-  }
-  body.appendChild(crt);
-  let crtClick = document.getElementsByClassName("click");
-  console.log(document.querySelector("div")[0]);
-  let close = document.getElementsByClassName("image close");
-  let pen = document.getElementsByClassName("image pen");
+function doIt () {
+  button.addEventListener("click", function () {
+    let crt;
+    let click;
 
-  //close
-  function myClick(some) {
-    for (i = 0; i < close.length; i++) {
-      close[i].addEventListener("click", function () {
-        crt[i].style.display = "none";
-      });
+    if (inputElement.value == "") {
+    } else {
+      crt = document.createElement("div");
+      crt.innerHTML = `${inputElement.value}<img class ='image close' src = 'https://cdn-icons-png.flaticon.com/128/1828/1828843.png'> <img class='image' src='https://cdn-icons-png.flaticon.com/128/5290/5290999.png'/> <img class='image pen' src='https://cdn-icons-png.flaticon.com/128/1250/1250615.png'/>`;
+      click = crt.className = "click";
+      body.appendChild(crt);
     }
-  }
-  myClick();
+    if (inputElement.value == inputElement.value) {
+      inputElement.value = "";
+    }
 
-  // let newDiv;
-  // for (i = 0; i < close.length; i++) {
-  //     pen[i].addEventListener("click" ,function() {
-  //         crt.innerHTML = `<input class='imageInput' value='${"f"}'><img class ='image close' src = 'https://cdn-icons-png.flaticon.com/128/1828/1828843.png'> <img class='image' src='https://cdn-icons-png.flaticon.com/128/5290/5290999.png'/> <img class='image pen' src='https://cdn-icons-png.flaticon.com/128/1250/1250615.png'/>`
-  // });
-
-  // }
-});
+    let crtClick = document.getElementsByClassName("click");
+    let close = document.getElementsByClassName("image close");
+    let pen = document.getElementsByClassName("image pen");
+    let jj = document.querySelector("div")
+    //close
+      for(i = 0; click[0].length; i++){
+        close[i].addEventListener("click" , function () {
+          
+        })
+      }
+    }
+)}
+doIt()
